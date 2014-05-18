58e0e3abe4fd36f86d719f2c20e0f8e7f91d4cc1
Max Tuni <maxilandia@gmail.com>
2014-05-18 11:47:04 +0200

    dropdown menu

---
+++ vendor/PHPStrap/Dropdown.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace PHPStrap;
+
+class Dropdown{
+	
+	private $Text, $Items = array();
+	
+	public function __construct($Text){
+		$this->Text = $Text;
+	}
+	
+	public function addItem($Content = ""){
+		$this->Items[] = Util\Html::tag("li", $Content);
+	}
+	
+	public function addDivider(){
+		$this->Items[] = Util\Html::tag("li", '', array('divider'));
+	}
+	
+ 	public function __toString(){
+        return Util\Html::tag("li", 
+        	$this->header() . Util\Html::tag("ul", implode($this->Items), array('dropdown-menu')), 
+        	array('dropdown')
+        );
+    }
+    
+    private function header(){
+    	return Util\Html::tag("a", 
+    		$this->Text . ' ' . Util\Html::tag("b", '', array('caret')),
+    		array('dropdown-toggle'), array('href' => '#', 'data-toggle' => 'dropdown')
+    	);
+    }
+	
+}
+
+?>
