4fc5e5113668edca894cfa2e86e6ca31347fd5a8
Max Tuni <maxilandia@gmail.com>
2014-11-23 13:14:21 +0100

    styles subtitle

--- vendor/PHPStrap/PageHeader.php
+++ vendor/PHPStrap/PageHeader.php
@@ -3,18 +3,19 @@ namespace PHPStrap;
 
 class PageHeader{
 	
-	private $Title, $Subtitle, $Styles;
+	private $Title, $Subtitle, $Styles, $SubtitleStyles;
 	
-	public function __construct($Title = "", $Subtitle = "", $Styles = array()){
+	public function __construct($Title = "", $Subtitle = "", $Styles = array(), $SubtitleStyles = array()){
 		$this->Title = $Title;
 		$this->Subtitle = $Subtitle;
+		$this->SubtitleStyles = $SubtitleStyles;
 		$this->Styles = array_merge(array('page-header'), $Styles);
 	}
 	
  	public function __toString(){
  		$elements = array($this->Title);
  		if(!empty($this->Subtitle)){
- 			$elements[] = Util\Html::tag("small", $this->Subtitle);
+ 			$elements[] = Util\Html::tag("small", $this->Subtitle, $this->SubtitleStyles);
  		}
  		return Util\Html::tag("div",
 			Util\Html::tag("h1",implode(" ", $elements)), 
