2075d00333f13e5d332fc3d8c4cfd552d50bb474
Max Tuni <maxilandia@gmail.com>
2015-10-30 20:48:41 +0100

    configurable panel styles

--- vendor/PHPStrap/Panel.php
+++ vendor/PHPStrap/Panel.php
@@ -18,17 +18,35 @@ class Panel{
 		$this->Headers[] = $Content;
 	}
 	
+	private $styles = array('panel', 'panel-default');
+	
+	public function addStyle($styleName){
+		$this->styles[] = $styleName;
+	}
+	
+	private $bodyStyles = array('panel-body');
+	
+	public function addBodyStyle($styleName){
+		$this->bodyStyles[] = $styleName;
+	}
+	
+	private $headingStyles = array('panel-heading');
+	
+	public function addHeadingStyle($styleName){
+		$this->headingStyles[] = $styleName;
+	}
+	
  	public function __toString(){
  		$header = !empty($this->Headers) ? 
- 			Util\Html::tag("div", implode($this->Headers), array('panel-heading')) : 
+ 			Util\Html::tag("div", implode($this->Headers), $this->headingStyles) : 
  			""
  		; 
  		return Util\Html::tag("div",
 			$header . Util\Html::tag("div",
 				implode($this->Contents), 
-				array('panel-body')
+				$this->bodyStyles
 			),
-			array('panel', 'panel-default')
+			$this->styles
 		);
     }
 	
